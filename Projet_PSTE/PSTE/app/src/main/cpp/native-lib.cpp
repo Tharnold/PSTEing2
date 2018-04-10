137a491b86d102c8a7849524d0881b2832c7593e
Thibault ARNOLD <thibault.arnold@edu.ece.fr>
2018-04-10 12:54:20 +0200

    Mise en place du projet AS

---
+++ Projet_PSTE/PSTE/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring
+
+JNICALL
+Java_com_arnold_thibault_pste_menu_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
